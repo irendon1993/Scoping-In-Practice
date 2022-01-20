d84384d1854c7dc6be2a486d56d25122796e2377
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-01-20 15:08:40 -0500

    Hoisting and TDZ

--- script.js
+++ script.js
@@ -1,33 +1,71 @@
 'use strict';
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
-    console.log(output);
+//   function printAge() {
+//     let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+//     console.log(output);
 
-    if (birthYear >= 1981 <= 1966) {
-      var millenial = true;
-      // Creating new varirable with same name
-      const firstName = 'Steven';
+//     if (birthYear >= 1981 <= 1966) {
+//       var millenial = true;
+//       // Creating new varirable with same name
+//       const firstName = 'Steven';
 
-      // Reassinging outer scope's variable
-      output = 'NEW OUTPUT';
-      const str = `Oh, and you're a millenial, ${firstName}`;
-      console.log(str);
+//       // Reassinging outer scope's variable
+//       output = 'NEW OUTPUT';
+//       const str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
 
-      function add(a, b) {
-        return a + b;
-      }
-    }
-    console.log(millenial);
-    console.log(output);
-  }
-  printAge();
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//     console.log(millenial);
+//     console.log(output);
+//   }
+//   printAge();
 
-  return age;
+//   return age;
+// }
+
+// const firstName = 'Isaac';
+// calcAge(1993);
+
+// Hoisiting with variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Isaac';
+let job = 'Student';
+const year = '1993';
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
 }
 
-const firstName = 'Isaac';
-calcAge(1993);
+const addEpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
