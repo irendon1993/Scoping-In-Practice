be0b2cd267a1ee2bb464a6038dbeda04ddc9b522
irendon1993 <38363661+irendon1993@users.noreply.github.com>
2022-01-20 14:33:33 -0500

    Reassining outer scopes

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
-    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 <= 1966) {
+      var millenial = true;
+      // Creating new varirable with same name
+      const firstName = 'Steven';
+
+      // Reassinging outer scope's variable
+      output = 'NEW OUTPUT';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(millenial);
     console.log(output);
   }
   printAge();
